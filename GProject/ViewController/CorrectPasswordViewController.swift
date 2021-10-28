f395f50bc85fec35d0df011dc365048d07e11b1a
hottinggot <hottinggot@naver.com>
2021-10-28 14:06:01 +0900

    Add correction page

--- GProject/ViewController/CorrectPasswordViewController.swift
+++ GProject/ViewController/CorrectPasswordViewController.swift
@@ -9,21 +9,48 @@ import UIKit
 
 class CorrectPasswordViewController: UIViewController {
 
+    let titleLabel = UILabel()
+    let passwordField = UITextField()
+    let confirmField = UITextField()
+    let button = UIButton()
+    
+    
+    lazy var vStackView: UIStackView = {
+        let stackV = UIStackView(arrangedSubviews: [self.titleLabel, self.passwordField, self.confirmField, self.button])
+        
+        stackV.spacing = 10
+        stackV.axis = .vertical
+        stackV.distribution = .fillProportionally
+        
+        return stackV
+    }()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        makeView()
+    
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func makeView() {
+        titleLabel.text = "비밀번호 변경"
+        passwordField.placeholder = "새 비밀번호"
+        confirmField.placeholder = "비밀번호 확인"
+        passwordField.borderStyle = .roundedRect
+        confirmField.borderStyle = .roundedRect
+        button.setTitle("확인", for: .normal)
+        button.backgroundColor = .black
+        button.tintColor = .white
+        
+        view.addSubview(vStackView)
+        
+        vStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        vStackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        vStackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        vStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        
     }
-    */
 
 }
